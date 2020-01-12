a56a98dea3c3124799903a41c43fc9190609e775
KariNoir <KariNoirm@gmail.com>
2020-01-12 04:13:40 -0500

    more changees to methods

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -2,18 +2,33 @@
 public class VirtualPetApp {
 
 	private int hunger = 0;
-	private int sleep = 0;
+	private int sleep = 10;
 	private int play = 5;
 	private int pot = 0;
 	
 	
-	public static void main(String[] args) {
+	public String getStatus() {
+		if(play <= 6) {
+			return "Let's play! ";
+		}
+		if(pot >=6) {
+			return "I got to go! ";
+		}
+		if(sleep >=6) {
+			return "Need Sleep! ";
+
+		}
+		if(hunger <=6) {
+			return "I need food!";
+		}
 		// Virtual Pet 
+		return null;
+
 	}
 		
 		public void eating() {
 			
-			hunger -= 5;
+			hunger -= 5; //decrease hunger by 2
 			if(hunger <0) { //if less than zero then it will be zero
 				hunger = 0;
 			}
@@ -27,7 +42,13 @@ public class VirtualPetApp {
 		
 		public void relief() {
 			pot = 0;
-			hunger -= 2;
+			if(pot <0) {
+				pot = 0;
+			}
+			if(pot >10) {
+				pot = 10;
+			}
+			hunger -= 2; //decrease hunger by 2
 			if(hunger > 10) {
 				hunger = 10;
 			}
@@ -35,11 +56,18 @@ public class VirtualPetApp {
 }
 
 		public void rest() {
-		
+			pot -= 2;
+			hunger += 2;
 			
+		if(sleep <0) {
+			sleep = 0;
+		}
 		}
 
 		public void fun() {
+			sleep -=3;
+			hunger -=2;
+			
 			
 		}
 			
