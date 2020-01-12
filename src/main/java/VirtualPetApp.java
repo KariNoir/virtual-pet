6a9ce755e09bc83271dbc44e4158d2ea8610b539
KariNoir <KariNoirm@gmail.com>
2020-01-12 03:17:56 -0500

    first steps for setting methods

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -3,10 +3,36 @@ public class VirtualPetApp {
 
 	public static void main(String[] args) {
 		// Virtual Pet 
-		
-		
-		
-
 	}
-
+		
+		public void eating() {
+			
+			
+		}
+		
+		
+		public void relief() {
+	
+	
 }
+
+		public void rest() {
+		
+			
+		}
+
+		public void fun() {
+			
+		}
+			
+		
+		public void time() {
+			
+		}
+		}
+
+
+
+		
+		
+
