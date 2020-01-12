3847ab8ff66beb3fcbdad5ce8333b33a5792ac5a
KariNoir <KariNoirm@gmail.com>
2020-01-12 02:16:25 -0500

    setup

---
+++ src/main/java/VirtualPetApp.java
@@ -0,0 +1,12 @@
+
+public class VirtualPetApp {
+
+	public static void main(String[] args) {
+		// Virtual Pet 
+		
+		
+		
+
+	}
+
+}
---
+++ src/main/java/VirtualPetAppTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import junit.framework.TestCase;
+
+@SuppressWarnings("unused")
+class VirtualPetAppTest {
+
+
+	@Test
+	void VirtualPetApp() {
+		assertEquals(  );
+	}
+}
