62ee7204ffa97db1c01e1b1ab727cc4cdb96efbb
KariNoir <KariNoirm@gmail.com>
2020-01-12 03:38:25 -0500

    more setup

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -1,19 +1,37 @@
 
 public class VirtualPetApp {
 
+	private int hunger = 0;
+	private int sleep = 0;
+	private int play = 5;
+	private int pot = 0;
+	
+	
 	public static void main(String[] args) {
 		// Virtual Pet 
 	}
 		
 		public void eating() {
 			
+			hunger -= 5;
+			if(hunger <0) { //if less than zero then it will be zero
+				hunger = 0;
+			}
+			if(hunger > 10) {
+				hunger = 10;
+			}
 			
+		
 		}
 		
 		
 		public void relief() {
-	
-	
+			pot = 0;
+			hunger -= 2;
+			if(hunger > 10) {
+				hunger = 10;
+			}
+			
 }
 
 		public void rest() {
